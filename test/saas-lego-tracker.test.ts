69f307838c49578576c838fe8e1b104ad807fd65
Jason Wadsworth <jasonwadsworth@outlook.com>
2023-12-14 20:07:42 -0600

    Initial commit

---
+++ test/saas-lego-tracker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SaasLegoTracker from '../lib/saas-lego-tracker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/saas-lego-tracker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SaasLegoTracker.SaasLegoTrackerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
